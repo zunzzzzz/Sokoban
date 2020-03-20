e8ebf64e8a66d80df2a79694ee4f7e17b1ea0b3d
zunzzz <a0983208130@gmail.com>
2020-03-20 10:48:42 +0800

    locate player and new state vector

--- hw1.cc
+++ hw1.cc
@@ -18,16 +18,23 @@ private:
 };
 int main(int argc, char** argv) {
     ifstream file(argv[1]);
+    vector<State> state;
     // initial State
-    State state;
+    State init_state;
     string tmp;
     while(getline(file, tmp)) {
-        state.map.push_back(tmp);
+        init_state.map.push_back(tmp);
     };
-    for(int height_iter = 0; height_iter < state.map.size(); height_iter++) {
-        for(int width_iter = 0; width_iter < state.map[height_iter].length(); width_iter++) {
-            cout << state.map[height_iter][width_iter];
+    for(int height_iter = 0; height_iter < init_state.map.size(); height_iter++) {
+        for(int width_iter = 0; width_iter < init_state.map[height_iter].length(); width_iter++) {
+            if(init_state.map[height_iter][width_iter] == 'o' || init_state.map[height_iter][width_iter] == 'O') {
+                init_state.player.x = width_iter;
+                init_state.player.y = height_iter;
+            }
+            cout << init_state.map[height_iter][width_iter];
         }
         cout << endl;
     }
+    cout << init_state.player.x << " " << init_state.player.y << endl;
+    state.push_back(init_state);
 }
