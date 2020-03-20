13fb1538cfe809b46301c86e4b1218b133057fcc
zunzzz <a0983208130@gmail.com>
2020-03-20 22:58:07 +0800

    check gameover and move function

--- hw1.cc
+++ hw1.cc
@@ -3,6 +3,11 @@
 #include <string>
 #include <vector>
 
+#define UP 0
+#define LEFT 1
+#define DOWN 2
+#define RIGHT 3
+
 using namespace std;
 class Obstacle {
 public:
@@ -22,8 +27,32 @@ public:
     Player player;
     vector<string> map;
     vector<Obstacle> obstacle;
+    bool is_legal = true;
 private:
 };
+State Move(int direction, State current_state) {
+    State new_state;
+    int x = current_state.player.x;
+    int y = current_state.player.y;
+    int one_step_x;
+    int one_step_y;
+    int two_step_x;
+    int two_step_y;
+    // cout << current_state.map[y][x] << endl;
+    if(direction == UP) {
+
+    }
+    else if(direction == LEFT) {
+
+    }
+    else if(direction == DOWN) {
+
+    }
+    else if(direction == RIGHT) {
+
+    }
+    return new_state;
+}
 int main(int argc, char** argv) {
     ifstream file(argv[1]);
     vector<State> all_state;
@@ -61,6 +90,30 @@ int main(int argc, char** argv) {
     cout << init_state.obstacle.size() << endl;
     all_state.push_back(init_state);
     vector<State> todo_queue(all_state);
-    cout << &todo_queue << endl;
-    cout << &all_state << endl;
+
+    while(true) {
+        // take and pop the last element
+        State current_state = todo_queue.back();
+        todo_queue.pop_back();
+        // check whether question is solved
+        int correct_count = 0;
+        for(int iter_ = 0; iter_ < current_state.obstacle.size(); iter_++) {
+            // right position -> "X"
+            if(current_state.obstacle[iter_].corret_position) {
+                correct_count++;
+            }
+        } 
+        if(correct_count == current_state.obstacle.size()) {
+            break;
+        }
+        for(int dir_iter = 0; dir_iter < 4; dir_iter++) {
+            State new_state;
+            new_state = Move(dir_iter, current_state);
+            if(new_state.is_legal) {
+                todo_queue.push_back(new_state);
+                all_state.push_back(new_state);
+            }
+        }
+    }
+    return 0;
 }
