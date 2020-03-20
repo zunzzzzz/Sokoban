e1f192eb0b40073d89fee58511b3e27de660e0ae
zunzzz <a0983208130@gmail.com>
2020-03-20 10:34:54 +0800

    update map and ignore

--- hw1.cc
+++ hw1.cc
@@ -1,4 +1,8 @@
-#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
 using namespace std;
 class Player {
 public:
@@ -6,11 +10,24 @@ public:
     int y;
 private:
 };
-class Map {
+class State {
 public:
     Player player;
+    vector<string> map;
 private:
 };
-int main() {
-
+int main(int argc, char** argv) {
+    ifstream file(argv[1]);
+    // initial State
+    State state;
+    string tmp;
+    while(getline(file, tmp)) {
+        state.map.push_back(tmp);
+    };
+    for(int height_iter = 0; height_iter < state.map.size(); height_iter++) {
+        for(int width_iter = 0; width_iter < state.map[height_iter].length(); width_iter++) {
+            cout << state.map[height_iter][width_iter];
+        }
+        cout << endl;
+    }
 }
